0659532ade3b88204f36c8f4cc8addccd793d9ba
V-Perotto <vittorio.perotto@outlook.com>
2023-11-09 01:32:38 -0300

    Adicionando iframe para Dashboard do Mongo

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import SideMenu from '../components/sideMenu';
 
-
 class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            process: localStorage.getItem("process"),
+            convenient: "",
+            title: "",
+            category: "",
+            priority: "",
+            value: "",
+            bought: ""
         }
-
     }
 
     render() {
@@ -21,8 +25,11 @@ class Home extends Component {
                 <SideMenu/>
 
                 <div className='content-page'>
-                    teste
+                    <h1 style={{alignItems:"center"}}>Lista de Itens</h1>
                 </div>
+                    <iframe style={{background: "#F1F5F4;", border: "none;", 
+                                    borderRadius: "2px;", boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2);"}}
+                            src="https://charts.mongodb.com/charts-liveasy-mzuoq/embed/dashboards?id=65497ed6-bd36-41cb-8bad-a98bf6888120&theme=light&autoRefresh=true&maxDataAge=1800&showTitleAndDesc=true"></iframe>
             </div>
 
 
